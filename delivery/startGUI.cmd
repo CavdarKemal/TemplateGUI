73a8bd4a88a73898beaeb011e76b9b9bca8a35cb
kemal <kemal@cavdar.de>
2026-01-05 11:25:39 +0100

    Start Script korrigiert

--- delivery/startGUI.cmd
+++ delivery/startGUI.cmd
@@ -2,7 +2,7 @@
 REM SET DEBUG_OPT=
 SET DEBUG_OPTS=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005
 SET JVM_ARGS=-Dfile.encoding=UTF8 -Xms512m -Xmx2g -XX:+HeapDumpOnOutOfMemoryError -classpath ".;lib/*"
-SET MAIN_CLASS=de.cavdar.gui.view.MainView
+SET MAIN_CLASS=de.cavdar.gui.Main
 SET AUFRUF=java %DEBUG_OPTS% %JVM_ARGS% %MAIN_CLASS% %CONFIG_FILE%
 ECHO Starting %MAIN_CLASS%...
 %AUFRUF%
